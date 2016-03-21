a6b9b2d140df864b619ad8eb5cf17a00c57ae2de
Kevin Saff <kevin.saff@gmail.com>
2016-03-21 00:54:08 -0400

    Added borg generator

--- maze_builder/main.py
+++ maze_builder/main.py
@@ -146,6 +146,7 @@ def main(args=None):
             ImageBlockIllustratorZoomed(wall_colors=[tuple(int(256*(1-random.random()**2)) for _ in range(3))], size=(506, 253)),
         ), 'multiply'): 19,
         CubicPovBuilder('boulders', CubicTemplateIllustrator('boulders.pov.jinja2'), 50): 25,
+        CubicPovBuilder('borg', CubicTemplateIllustrator('borg.pov.jinja2'), 8, 8, 8): 25,
     },
         default_status=LostTextWriter().write,
         args=args
