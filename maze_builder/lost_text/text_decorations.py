a3a4d08af1422c77dba79ad12e09a0dc5edbd913
Kevin Saff <kevin.saff@gmail.com>
2016-03-26 17:41:18 -0400

    More macgyvering.

--- maze_builder/lost_text/text_decorations.py
+++ maze_builder/lost_text/text_decorations.py
@@ -1,5 +1,44 @@
 import random
+import re
 from maze_builder.sewer import Choice
+
+UNCHANGING_PLURALS = {
+    'bison', 'buffalo', 'deer', 'fish', 'moose', 'pike', 'plankton', 'salmon', 'sheep', 'squid', 'swine', 'trout',
+}
+UNCHANGING_PLURALS_BY_SIZE = {
+    i: {plural for plural in UNCHANGING_PLURALS if len(plural)==i}
+    for i in range(1, 1+max(len(plural) for plural in UNCHANGING_PLURALS))
+}
+
+IRREGULAR_PLURALS = {
+    'man': 'men',
+    'child': 'children',
+    'foot': 'feet',
+    'goose': 'geese',
+    'tooth': 'teeth',
+    'mouse': 'mice',
+    'person': 'people',
+    'leaf': 'leaves',
+}
+IRREGULAR_PLURALS_BY_SIZE = {
+    i: {sing: plural for sing, plural in IRREGULAR_PLURALS.items() if len(sing)==i}
+    for i in range(1, 1+max(len(sing) for sing in IRREGULAR_PLURALS))
+}
+LONE_IRREGULAR_PLURALS = {
+    'ox': 'oxen',     # box -> boxes
+    'louse': 'lice',  # blouse -> blouses
+}
+
+PLURALIZERS = [
+    (re.compile(pattern), replacement)
+    for (pattern, replacement) in [
+        (r'(ies)$', r'\1'),
+        (r'(\w+)((\s+|-)(of|from|about)(\s+|-)\w+)$', lambda match: pluralize(match.group(1)) + match.group(2)),
+        (r'(s|x|sh|ch)$', r'\1es'),
+        (r'([b-df-hj-np-tv-z])y$', r'\1ies'),
+        (r'quy$', r'quies'),
+        ('$', 's'),
+]]
 
 
 SENTENCE_FIXES = {
@@ -22,6 +61,61 @@ STRIKES = Choice({
     '\u0337': 3,   # Short angled stroke
     '\u0335': 1,   # Short horiz stroke
 })
+
+
+def pluralize(item):
+    """
+
+    >>> pluralize('species')
+    'species'
+    >>> pluralize('bus')
+    'buses'
+    >>> pluralize('soliloquy')
+    'soliloquies'
+    >>> pluralize('box')
+    'boxes'
+    >>> pluralize('fish')
+    'fish'
+    >>> pluralize('starfish')
+    'starfish'
+    >>> pluralize('ash')
+    'ashes'
+    >>> pluralize('latch')
+    'latches'
+    >>> pluralize('baby')
+    'babies'
+    >>> pluralize('cow')
+    'cows'
+    >>> pluralize('piece of eight')
+    'pieces of eight'
+    >>> pluralize('box of destiny')
+    'boxes of destiny'
+    >>> pluralize('fish of destiny')
+    'fish of destiny'
+    >>> pluralize('starfish of destiny')
+    'starfish of destiny'
+    >>> pluralize('woman of destiny')
+    'women of destiny'
+    """
+    if item in LONE_IRREGULAR_PLURALS:
+        return LONE_IRREGULAR_PLURALS[item]
+    if item in IRREGULAR_PLURALS:
+        return IRREGULAR_PLURALS[item]
+    if item in UNCHANGING_PLURALS:
+        return item
+    if ' ' not in item:
+        lower = item.lower()
+        for size, words in IRREGULAR_PLURALS_BY_SIZE.items():
+            if lower[-size:] in words:
+                return item[:-size] + words[lower[-size:]]
+        for size, words in UNCHANGING_PLURALS_BY_SIZE.items():
+            if lower[-size:] in words:
+                return item
+
+    for pattern, replacement in PLURALIZERS:
+        if pattern.search(item):
+            return pattern.sub(replacement, item)
+    return item
 
 
 def strike_all(text):
@@ -48,3 +142,6 @@ def fix_sentence(sentence):
 
 if __name__ == '__main__':
     print(fix_sentence('Maybe I can build a amazing GPS from 3 threads.'))
+
+    import doctest
+    doctest.testmod()
